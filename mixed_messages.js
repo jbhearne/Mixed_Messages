42ff9b3f24ee766512999b767b777a846ea9d13c
JBHearne <95382527+jbhearne@users.noreply.github.com>
2022-01-19 18:20:04 -0600

    comment and clean

--- mixed_messages.js
+++ mixed_messages.js
@@ -13,6 +13,7 @@ const quotes = {
     variation2: [],
     variation3: [],
     cutOrder(randOrNum) {
+        //adjusts "cut" array to vary the order of keywords either randomly or by set number
         let shifter = 0;
         if (randOrNum === 'r' || randOrNum === 'random') {
             shifter = Math.floor(Math.random() * this.cut.length)
@@ -24,26 +25,22 @@ const quotes = {
             this.cut.push(this.cut.shift())
         }
     },
-    parseWhole(str) {return str.split('\n')},
-    parseQuote(str) { // .indexOf(cut)?
+    parseWhole(str) {return str.split('\n')}, //returns the individual lines of text from a larger block of quotes
+    parseQuote(str) { 
+        //Cuts original quote into 3 parts and stores them in 3 different arrays.
         let cutIndex = 0
         let endIndex = 0
         const endTerm = '" -'
         let started = ''
         let ended = ''
         let personed = ''
-        //console.log(str);
         for (let i = 0; i < this.cut.length; i++) {
-            //console.log(str);
+            
             if (str.includes(this.cut[i])) {
-                //console.log(this.cut[i]);
-                //console.log(str.indexOf(this.cut[i]))
                 cutIndex = str.indexOf(this.cut[i]) + this.cut[i].length;
                 started = str.slice(0, cutIndex);
-                //console.log(started)
                 break;
             } else if (i === this.cut.length - 1) {
-                console.log(1111)
                 cutIndex = str.indexOf(' ', Math.floor(str.length / 2) - 10) + 1;
                 started = str.slice(0, cutIndex)
                 break; 
@@ -54,16 +51,20 @@ const quotes = {
             ended = str.slice(cutIndex, endIndex);
             personed = str.slice(endIndex, str.length);
         } else {
-            //if there is formatting or parsing error
+            console.log('Not formatted in famous quote language.')
         }
         this.start.push(started);
         this.end.push(ended);
         this.person.push(personed);
     },
     randQuote(arr) {
+        //returns a single string quote sellected randomly from array
         const randIndex = Math.floor(Math.random() * arr.length);
-        return arr.splice(randIndex, 1)[0]/*single quote. .splice() this.whole?*/},
-    randParse(arr1, arr2, arr3) {/*newquote. needs to know not to select parts of the same quote*/
+        return arr.splice(randIndex, 1)[0]}, //note: index [0] used to convert the single ellement array to a string.
+    randParse(arr1, arr2, arr3) {
+        //randomizes 3 original quotes to create 3 new quotes each quote containing a differnt part of the original.
+        //then assigns them to arrays (variation#) so that the matched indexes will always provide complimentary quotes
+        //returns the same value as variation1
         let startNum = Math.floor(Math.random() * arr1.length);
         let endNum = Math.floor(Math.random() * arr2.length);
         let personNum = Math.floor(Math.random() * arr3.length);
@@ -108,6 +109,7 @@ const quotes = {
         return arr1[startNum] + arr2[endNum] + arr3[personNum];
     },
     quoteCutter(num) {
+        //runs parseQuote at least 3 times or num times so there are at least 3 quotes to reconstitue
         let checkNum = num
         if (num < 3) {
             checkNum = 3;
@@ -115,7 +117,6 @@ const quotes = {
             checkNum = this.whole.length;
         }
         for (let i = 0; i < checkNum; i++) {
-            console.log(i)
             quotes.parseQuote(quotes.randQuote(quotes.whole))
         }
     }
